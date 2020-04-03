c1e09ccee28f516f6d76af3683d9f97940113a20
Justin Zhao <40444147+zhaojustin@users.noreply.github.com>
2020-04-02 20:22:02 -0700

    created new layout for score input

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@ var tests;
 
 //initialize dropdowns
 $('#score-select').dropdown({
-    onChange: function() {
+    onChange: function () {
         tests = $('#score-select').dropdown('get value');
-        addScoreInput();
+        addScoreInput(tests[tests.length-1]);
     }
 });
 
+$('.ui.dropdown').dropdown();
+
 //create input and label every time new input is added
-function addScoreInput() {
-    
+function addScoreInput(test_name) {
+    var newInput = document.createElement('select');
+    newInput.setAttribute('test_name', 'empTable');
 }
 
+
+function func() {
+    alert( $('#art_history').dropdown('get text'));
+}
 
